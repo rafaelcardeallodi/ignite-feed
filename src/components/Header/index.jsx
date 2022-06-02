f9a55099cb8cac436f63f06f7e2fbb739cae7ae7
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-06-02 18:43:39 -0300

    Componente: Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import styles from "./styles.module.css";
 
+import igniteLogo from "../../assets/ignite-logo.svg";
+
 export function Header() {
   return (
-    <header>
-      <strong>Ignite Feed</strong>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
