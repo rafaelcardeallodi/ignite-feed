fbf28687614d6ecbd1e7bf0c07e8b3e50720df67
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-06-02 19:21:32 -0300

    Finalizando Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./styles.module.scss";
 
 export function Sidebar() {
@@ -9,12 +11,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/rafaelcardeallodi.png"
+        />
         <strong>Rafael Lodi</strong>
-        <span>Web Developer</span>
+        <span>Front-End Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
