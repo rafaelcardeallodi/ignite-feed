0780387e8a81db569dd59fd969c8664457ca188c
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-06-05 18:41:43 -0300

    Iterando no JSX

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,38 +2,22 @@ import { Avatar } from "../Avatar";
 import { Comment } from "../Comment";
 import styles from "./styles.module.scss";
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
   return (
     <article className={styles.container}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/rafaelcardeallodi.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.author__info}>
-            <strong>Rafael Lodi</strong>
-            <span>Front-end Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time dateTime="2022-06-04 18:00:00">Publicado há 1h</time>
       </header>
 
-      <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-
-        <p>
-          <a href="#">jane.design/doctorcare</a>
-        </p>
-
-        <p>
-          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
-          <a href="#">#rocketseat</a>
-        </p>
-      </div>
+      <div className={styles.content}></div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
